5c41d85a2fa5ef3290c3ac485d9fb99dc7ddaf9f
RFSurdsman <ryan_fallah@hotmail.com>
2019-01-30 17:37:05 +1100

    Basic Web Server

--- dist/server.js
+++ dist/server.js
@@ -1,2 +1,17 @@
-console.log("Master Server started.");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const mustacheExpress = require("mustache-express");
+const path = require("path");
+const app = express();
+app.engine('html', mustacheExpress());
+app.set('view engine', 'html');
+app.set('views', path.join(__dirname, '../views'));
+app.use('/static', express.static(path.join(__dirname, '../static')));
+app.get('/', function (req, res) {
+    res.render('index', { root: path.join(__dirname, '../lib') });
+});
+app.listen(3000, function () {
+    console.log('Racerr.io Master Server started.');
+});
 //# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,18 @@
-console.log("Master Server started.");
+import express = require('express');
+import mustacheExpress = require('mustache-express');
+import path = require('path');
+
+const app = express();
+
+app.engine('html', mustacheExpress());
+app.set('view engine', 'html');
+app.set('views', path.join(__dirname, '../views'));
+app.use('/static', express.static(path.join(__dirname, '../static')));
+
+app.get('/', function(req, res) {
+  res.render('index', { root: path.join(__dirname, '../lib') });
+});
+
+app.listen(3000, function() {
+  console.log('Racerr.io Master Server started.');
+});
