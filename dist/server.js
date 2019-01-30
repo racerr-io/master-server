6c779f540bed991e75ffb6515e8c908c45a10671
RFSurdsman <ryan_fallah@hotmail.com>
2019-01-30 21:02:11 +1100

    Massive upgrades

--- dist/server.js
+++ dist/server.js
@@ -4,14 +4,10 @@ const express = require("express");
 const mustacheExpress = require("mustache-express");
 const path = require("path");
 const app = express();
-app.engine('html', mustacheExpress());
-app.set('view engine', 'html');
-app.set('views', path.join(__dirname, '../views'));
-app.use('/static', express.static(path.join(__dirname, '../static')));
-app.get('/', function (req, res) {
-    res.render('index', { root: path.join(__dirname, '../lib') });
-});
-app.listen(3000, function () {
-    console.log('Racerr.io Master Server started.');
-});
+app.engine("html", mustacheExpress());
+app.set("view engine", "html");
+app.set("views", path.join(__dirname, "../views"));
+app.use("/static", express.static(path.join(__dirname, "../static")));
+app.get("/", (_, res) => res.render("index"));
+app.listen(80, () => console.log("racerr.io Master Server started."));
 //# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,17 @@
-import express = require('express');
-import mustacheExpress = require('mustache-express');
-import path = require('path');
+import express = require("express");
+import mustacheExpress = require("mustache-express");
+import path = require("path");
 
 const app = express();
+app.engine("html", mustacheExpress());
+app.set("view engine", "html");
+app.set("views", path.join(__dirname, "../views"));
+app.use("/static", express.static(path.join(__dirname, "../static")));
 
-app.engine('html', mustacheExpress());
-app.set('view engine', 'html');
-app.set('views', path.join(__dirname, '../views'));
-app.use('/static', express.static(path.join(__dirname, '../static')));
+app.get("/", (_, res) =>
+  res.render("index")
+);
 
-app.get('/', function(req, res) {
-  res.render('index', { root: path.join(__dirname, '../lib') });
-});
-
-app.listen(3000, function() {
-  console.log('Racerr.io Master Server started.');
-});
+app.listen(80, () =>
+  console.log("racerr.io Master Server started.")
+);
