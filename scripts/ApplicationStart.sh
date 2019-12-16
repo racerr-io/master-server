5191576f15f810d0df8a48ce1889e121c04c9a37
Ryan Fallah <ryan_fallah@hotmail.com>
2019-12-16 22:12:21 +1100

    Update Scripts

--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -1,3 +1,4 @@
+source /home/ec2-user/.bash_profile
 sudo iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 3000
 pm2 start /home/ec2-user/master-server/dist/server.js --name "Racerr Master Server"
 #pm2 start "/home/ec2-user/master-server/unity-bin/Linux Server Build/Racerr.x86_64" --name "Racerr Game Server Instance"
--- scripts/ApplicationStop.sh
+++ scripts/ApplicationStop.sh
@@ -1,2 +1,3 @@
+source /home/ec2-user/.bash_profile
 pm2 delete all
 exit 0
